ed555647570a1c9c411281e89b52f9185329fad9
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-03 15:47:50 +0200

    ADD let name

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+// - Richiesta il nome utente
+//     - dichiarare una variabile
+        let userName; 
+//     - assegnare alla variabile il valore restituito da un prompt
+        userName = prompt ("Ciao, come ti chiami?");
+        console.log(userName);
+
+// - Richiesta il cognome
+//     - dichiarare una variabile
+//     - assegnare alla variabile il valore restituito da un prompt
+// - Richiesta il colore preferito
+//     - dichiarare una variabile
+//     - assegnare alla variabile il valore restituito da un prompt
+// - Assegnazione password 
+//     - dichiarare una variabile (parte numerico del password) 
+//     - dichiarare una variabile del password
+// - Invio password al utente
